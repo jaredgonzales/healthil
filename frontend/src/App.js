1798f85994be756f3bed280a60763b9581cc4f51
Jared Gonzales <jrg2221@columbia.edu>
2021-11-16 15:21:05 -0500

    Updated frontend - initial form layout
    Needs to be modular

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HealthIL from React!
-        </p>
-        <p>Welcome!</p>
+        <h2>
+          HealthIL Startup Assessment Form
+        </h2>
       </header>
+	  <div className="App-body">
+		<div className="Card">
+			<form>
+			<label>Contact Name
+				<input name="contact_name" type="text" />
+			</label>
+			<br />
+			<label> Company Name
+				<input name="company_name" type="text" />
+			</label>
+			<br />
+			<label> Collaboration Desired
+				<select>
+					<option value="observational">Observational Study/Research</option>
+					<option value="poc">POC/MVP</option>
+					<option value="pilot">Pilot</option>
+					<option value="pilot_expansion"> Pilot Expansion (2nd site)</option>
+					<option value="commercialization">Commercialization/Pre-commercialization</option>
+				</select>
+			</label>
+			<br />
+			<input type="submit" value="Submit" />
+		</form>
+		</div>
+	  </div>
     </div>
   );
 }

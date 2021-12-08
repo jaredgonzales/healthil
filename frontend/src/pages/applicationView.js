36f853998c57297990bbba7c310d1c7abf7da963
Jared Gonzales <jrg2221@columbia.edu>
2021-12-07 21:27:41 -0500

    Design Internal Assessment view
    Added ApplicationView component into the page

--- frontend/src/pages/applicationView.js
+++ frontend/src/pages/applicationView.js
@@ -1,22 +1,40 @@
-import {Header} from './../components/core';
+import { Header } from './../components/core';
 
 import './../App.css';
 import './../css/form.css';
 
-function ApplicationView() {
+function ApplicationView(props) {
+	var applicationName = props.application
+	var applicationData = fetch("/readApplication?company_name=" + { applicationName })
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			return data;
+		});
+
+	var company_name = applicationData.company_name;
+	var contact_name = applicationData.contact_name;
+
 	return (
-		<div className="App">
-			<Header title="{Company Name} Application" />
-			<div className="App-body">
-				<div className="Card">
-					<div class="field">
-                        <h2>Field 1</h2>
-                    </div>
-						
-                    <div class="field">
-                        <h2>Field 2</h2>
-                    </div>
-						
+		<div className="Card">
+			<div class="input-form">
+				<h1>Submitted Application</h1>
+				<div class="field">
+					<label for="example-field">Example label</label>
+					<br />
+					<input id="example-field" type="text" placeholder="InputHere Incorporated" disabled></input>
+				</div>
+				<div class="field">
+					<label for="example-field2">Second Example label</label>
+					<br />
+					<input id="example-field2" type="text" placeholder="FullName GuyHere" disabled></input>
+				</div>
+				<div class="field">
+					<h2>{company_name}</h2>
+				</div>
+
+				<div class="field">
+					<h2>{contact_name}</h2>
 				</div>
 			</div>
 		</div>
